e3fe8ce0c4e55dfc63c2a0ee7599aeb70c3a5452
nadiraaraa <nadira@clevero.co>
2025-04-14 16:12:18 +0700

    implement read, open, and display folders and files

---
+++ src/components/Folder.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useState } from 'react'
+
+const Folder = ({dir, files, openItem}) => {
+    const [sortOption, setSortOption] = useState('');
+    const dirPaths = dir.split('/');
+    const dirDepth = dirPaths.length;
+    const title = dirPaths[dirDepth - 1];
+
+    const handleSortChange = (e) => {
+        setSortOption(e.target.value);
+    };
+
+    const sortFiles = () => {
+        switch (sortOption) {
+          case 'name':
+            return [...files].sort((a, b) => a.name.localeCompare(b.name));
+          case 'size':
+            return [...files].sort((a, b) => a.size - b.size);
+          case 'date':
+            return [...files].sort((a, b) => a.createdAt - b.createdAt);
+          default:
+            return files;
+        }
+      };
+
+  return (
+    <div className='border border-gray-400 h-full w-72 overflow-y-hidden'>
+            <div className='Header flex justify-between w-full p-4 bg-gray-500'>
+                <div>{dir == '.' ? 'Root' : title}</div>
+                <div>
+                    {/* <label htmlFor='sort'>Sort by</label> */}
+                    <select
+                        id='sort-options'
+                        value={sortOption}
+                        onChange={handleSortChange}
+                        className=''
+                    >
+                        <option value="">Sort by</option>
+                        <option value="name">Name</option>
+                        <option value="size">Size</option>
+                        <option value="date">Date</option>
+                    </select>
+                </div>
+            </div>
+            <div className='p-4 h-full overflow-y-auto'>
+                <ul>
+                    {sortFiles(files).map((item => (
+                        <li key={item.path} 
+                            onDoubleClick={e => openItem(item, dirDepth)}
+                            className='hover:bg-sky-700'
+                        >
+                            {item.isDirectory ? '[D]' : '[F]'} {item.name}
+                        </li>
+                    )))}
+                </ul>
+            </div>
+    </div>
+  )
+}
+
+export default Folder;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Folder from '../components/Folder'
+import FileInfo from '../components/FileInfo'
+
+const Home = () => {
+    const homeDir = '.';
+    const [folderTree, setFolderTree] = useState([{ dir: homeDir, isDir: true, files: []}]);
+
+    useEffect(() => {
+        const fetchFiles = async (directory) => {
+            try {
+                const fileList = await window.electron.getFiles(directory); 
+                setFolderTree((prevTree) => {
+                    const updatedTree = [...prevTree];
+                    updatedTree[updatedTree.length - 1].files = fileList; 
+                    return updatedTree;
+                });
+            } catch (error) {
+                console.error('Failed to fetch files:', error);
+            }
+        };
+
+        if (folderTree[folderTree.length-1].isDir && folderTree[folderTree.length-1].files.length == 0){
+            const lastDirectory = folderTree[folderTree.length - 1].dir;
+            fetchFiles(lastDirectory);
+        }
+    }, [folderTree]); 
+
+    const openItem = (item, depth) => {
+        setFolderTree((prevTree) => [
+            ...prevTree.slice(0, depth),
+            {
+              dir: './' + item.path,
+              isDir: item.isDirectory,
+              ...(item.isDirectory
+                ? { files: [] }
+                : { info: item })
+            }
+          ]);
+    }
+
+    const handleBack = () => {
+        setFolderTree((prevTree) => prevTree.slice(0, prevTree.length - 1));
+    }
+
+
+    return (
+        <div className='Home h-screen flex flex-col'>
+            <div className='Header flex p-3 gap-x-4 bg-gray-700 w-screen items-center'>
+                <div onClick={handleBack} className='bg-gray-600 p-2 rounded-md'>Back</div>
+                <div>Documents</div>
+            </div>
+            <div className='Folders h-full overflow-x-auto'>
+                <div className='flex h-full'>
+                    {folderTree.map((item, index) => (
+                        item.isDir ? (
+                            <div key={index}>
+                                <Folder dir={item.dir} files={item.files} openItem={openItem}/>
+                            </div>
+                        ) : (
+                            <div key={index}>
+                                <FileInfo file={item.info} />
+                            </div>
+                        )
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
